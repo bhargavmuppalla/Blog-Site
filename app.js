b426dd546a8727312ac535210928da8ac438d445
Bhargav Muppalla <bhargavmuppalla@Bhargavs-Air.wifi.gsu.edu>
2022-08-31 15:24:31 -0400

    corrected port command

--- app.js
+++ app.js
@@ -98,6 +98,6 @@ app.post("/compose", function(req, res) {
 
 
 
-app.listen(process.ENV.PORT || 3001, function() {
+app.listen(process.env.PORT || 3001, function() {
   console.log("Server started on port 3001");
 });
